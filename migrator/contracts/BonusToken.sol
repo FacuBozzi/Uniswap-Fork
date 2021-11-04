c7a517b49dab973b60dc61ac72a295719f39f7dd
facundo bozzi <facundo.bo@hotmail.com>
2021-11-04 00:58:00 -0300

    inheriting openzeppelin and uniswap contracts

---
+++ migrator/contracts/BonusToken.sol
@@ -0,0 +1,22 @@
+pragma solidity =0.6.6;
+
+import @openzeppelin/contracts/token/ERC20/ERC20.sol;
+
+contract BonusToken is ERC20 {
+    address public admin;
+    address public liquidator;
+
+    constructor() ERC20("WOLFGANG", "WLFG") public {
+        admin = msg.sender;
+    }
+    function setLiquidator(address _liquidator) external {
+        require(msg.sender == admin, "Only admin can set the liquidator");
+        liquidator = _liquidator;
+        admin = liquidator;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, "Only liquidator can mint");
+        _mint(to, amount);
+    }
+}
