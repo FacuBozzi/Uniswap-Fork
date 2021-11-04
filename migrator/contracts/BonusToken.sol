2ee1efac8e8d133c622724bb9e2f074648ee7095
facundo bozzi <facundo.bo@hotmail.com>
2021-11-04 01:53:03 -0300

    debugging done

--- migrator/contracts/BonusToken.sol
+++ migrator/contracts/BonusToken.sol
@@ -1,12 +1,12 @@
-pragma solidity =0.6.6;
+pragma solidity >=0.6.6;
 
-import @openzeppelin/contracts/token/ERC20/ERC20.sol;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BonusToken is ERC20 {
     address public admin;
     address public liquidator;
 
-    constructor() ERC20("WOLFGANG", "WLFG") public {
+    constructor() ERC20("WOLFGANG", "WLFG") {
         admin = msg.sender;
     }
     function setLiquidator(address _liquidator) external {
